dc2b3e8bbbcf42372e4faba32e5e06925f0aab96
Ervin Hodzic <ervinhodzic1234@gmail.com>
2025-03-02 14:33:45 +0100

    Update API url

--- front/src/config/APIUrl.js
+++ front/src/config/APIUrl.js
@@ -1,7 +1,7 @@
 const API_URL =
-import.meta.VITE_REACT_APP_API_URL || 
-  (import.meta.VITE_NODE_ENV === "production"
-    ? import.meta.VITE_REACT_APP_API_URL
+import.meta.env.VITE_REACT_APP_API_URL || 
+  (import.meta.env.VITE_NODE_ENV === "production"
+    ? import.meta.env.VITE_REACT_APP_API_URL
     : "http://localhost:5000/api"); // Fallback for development
 
 export default API_URL;
